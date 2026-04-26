250d285aac9a63d44df4f59b9081ac8cab5aec9f
mobilesikuda <ya.sikuda@yandex.ru>
2026-04-27 00:14:46 +0300

    libs up

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -62,6 +62,7 @@ android {
     defaultConfig {
         applicationId = "ru.sikuda.mobile"
         minSdk = libs.versions.android.minSdk.get().toInt()
+        //noinspection OldTargetApi
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
